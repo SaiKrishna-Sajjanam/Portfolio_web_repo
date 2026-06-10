b10317249a53c1150280bfdc59dae6d53c8afa0a
SaiKrishna-Sajjanam <37737079+SaiKrishna-Sajjanam@users.noreply.github.com>
2026-06-10 02:11:53 +0100

    fix: add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/Portfolio_web_repo/",
+    server: {
+          host: "::",
+          port: 8080,
     },
-  },
-
+    plugins: [react()],
+    resolve: {
+          alias: {
+                  "@": path.resolve(__dirname, "./src"),
+          },
+    },
 });
